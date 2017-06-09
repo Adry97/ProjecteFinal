cdaa6ab31fab743030cf006568142c9127167f4f
Adry97 <aps.vng.1997@gmail.com>
2017-06-09 15:48:25 +0200

    Add files via upload
    arbre cambiado como dijimos,no funciona aun

--- ArbreEvolutiu.java
+++ ArbreEvolutiu.java
@@ -6,7 +6,6 @@
 package practica;
 
 import java.util.ArrayList;
-import java.util.StringJoiner;
 
 /**
  *
@@ -18,36 +17,66 @@ public class ArbreEvolutiu {
   
     
     
+    @SuppressWarnings("empty-statement")
     public ArbreEvolutiu(int mat[][],ArrayList<Genoma> a){
         int b=2;
         Genoma ex = new Genoma("hola","adeu");
     
-       BinaryTree arbresq=new BinaryTree(ex);
+       /*BinaryTree arbresq=new BinaryTree(ex);
        BinaryTree arbredret=new BinaryTree(ex);
        BinaryTree arbre=new BinaryTree(ex,arbresq,arbredret);
        arbre.value(ex);
-        
-        int i=0;
-        int j=0;
+       */
+
         int nivells=0;
-        while(nivells<5){
+        /*while(nivells<5){
             arbre.setRightChild(arbredret);
             arbre.setLeftChild(arbresq);
+        }*/
+       // System.out.println(arbre);
+       int k=0;
+       int var2=2000;
+       int aux=10000;
+       ArrayList nums = new ArrayList(13);
+       int[] num= new int[13];
+       for(int i=0;i<a.size();i++){
+            for(int j=0;j<a.size();j++){
+                if(mat[i][j]!=0){
+                    int m=mat[i][j];
+                    aux=Math.min(aux,m);
+                    //System.out.println("buccca");
+                    if(j==12){
+                        System.out.println("hola");
+                        if(num[0]!=0){
+                            for(int z=0;z<a.size();++z){
+                                  int var=num[z];
+                              var2= Math.min(var, var2);
+                             }
+                            //System.out.print(num[0]);
+                        }
+                        num[k]=var2;
+                        System.out.println(num[k]);
+                        ++k;
+                    }  
+                }
+                else ++j;
+
+            }       
         }
-        System.out.println(arbre);
-       /* for(i=0;i<13;i++){
-            for(j=0;j<13;j++)
-                arbre.value(mat[i][j]);
-                arbre.setRightChild(arbredret);
-                arbre.setLeftChild(arbresq);
+       System.out.println("aquii");
+       for (int x=0;x<nums.size();++x){
+           System.out.print(num[x]);
+           
+       }
+                    
                 
-         */   
+            
             
     
     }
 
             
-        }
+}
 
     
     
